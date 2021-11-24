96652836452e711377d7ccbef089d8940f174e47
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-11-24 11:18:18 -0500

    Add files via upload

---
+++ src/Ex6_1_2_PrintOddIndexes.java
@@ -0,0 +1,12 @@
+
+public class Ex6_1_2_PrintOddIndexes {
+	public static void main(String[] args) {
+		int[] oddIndexArray = new int[] { 1, 2, 3, 4, 5 };
+		printOddIndices(oddIndexArray);
+	}
+
+	public static void printOddIndices(int[] arr) {
+		// your code goes here!
+	}
+
+}
---
+++ src/Ex6_1_3_WinnersAndLosers.java
@@ -0,0 +1,15 @@
+
+public class Ex6_1_3_WinnersAndLosers {
+	static String[] teams = { "Maryland", "Michigan State", "Minnesota", "Iowa", "Michigan", "Penn State", "Purdue",
+			"Ohio State", "Rutgers", "Indiana", "Northwestern", "Wisconson", "Detroit", "Pittsburgh" };
+
+	static int[] scores = { 21, 40, 22, 27, 21, 17, 31, 59, 38, 3, 7, 35, 16, 16 };
+
+	public static void main(String[] args) {
+		//Write your code here
+	
+	}
+	
+	
+	
+}
