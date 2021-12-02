bbada1f8642a6fb1f5503cadf86e9ed461b83be0
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-12-02 13:13:38 -0500

    Update Ex6_1_3_WinnersAndLosers.java

--- src/Ex6_1_3_WinnersAndLosers.java
+++ src/Ex6_1_3_WinnersAndLosers.java
@@ -1,7 +1,7 @@
 
 public class Ex6_1_3_WinnersAndLosers {
 	static String[] teams = { "Maryland", "Michigan State", "Minnesota", "Iowa", "Michigan", "Penn State", "Purdue",
-			"Ohio State", "Rutgers", "Indiana", "Northwestern", "Wisconson", "Detroit", "Pittsburgh" };
+			"Ohio State", "Rutgers", "Indiana", "Northwestern", "Wisconsin", "Detroit", "Pittsburgh" };
 
 	static int[] scores = { 21, 40, 22, 27, 21, 17, 31, 59, 38, 3, 7, 35, 16, 16 };
 
