d7f4730beeb0bebf976925a0b8b39b3d88507d52
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-11-28 11:40:41 -0500

    Update Ex6_1_2_PrintOddIndexes.java

--- src/Ex6_1_2_PrintOddIndexes.java
+++ src/Ex6_1_2_PrintOddIndexes.java
@@ -1,7 +1,7 @@
 
 public class Ex6_1_2_PrintOddIndexes {
 	public static void main(String[] args) {
-		int[] oddIndexArray = new int[] { 1, 2, 3, 4, 5 };
+		int[] oddIndexArray = new int[] { 1, 2, 3, 4, 5, 4, 3, 2, 1};
 		printOddIndices(oddIndexArray);
 	}
 
